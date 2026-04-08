fd30d4ef37e20556d1930ca667ffd27ddadaaf7d
waqas-404 <kareemwaqas1@gmail.com>
2026-04-08 18:25:42 +0500

    new

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import pickle
 import cloudpickle
-# from utils import transform_text
+from utils import transform_text
 
-# transform_text = pickle.load(open('transform_text.pkl','rb'))
-with open('transform_text.pkl', 'rb') as f:
-    transform_text = cloudpickle.load(f)
+transform_text = pickle.load(open('transform_text.pkl','rb'))
+# with open('transform_text.pkl', 'rb') as f:
+#     transform_text = cloudpickle.load(f)
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model= pickle.load(open('model.pkl','rb'))
 
