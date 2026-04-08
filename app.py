31ccf0f277bf94cf2769a9edbd47826b7dbd97fa
waqas-404 <kareemwaqas1@gmail.com>
2026-04-08 17:37:35 +0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import cloudpickle
+# from utils import transform_text
+
+# transform_text = pickle.load(open('transform_text.pkl','rb'))
+with open('transform_text.pkl', 'rb') as f:
+    transform_text = cloudpickle.load(f)
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model= pickle.load(open('model.pkl','rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+input_text = st.text_area("Enter your message:")
+
+if st.button('Predict'):
+    transformed_text = transform_text(input_text)
+
+    transformed_text_v = tfidf.transform([transformed_text])
+
+    probs = model.predict_proba(transformed_text_v)[:,1]
+    result = (probs > 0.3).astype(int)
+    if result == 1:
+        st.header('Spam')
+    else:
+        st.header("Not Spam")
---
+++ utils.py
@@ -0,0 +1,31 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+import string
+
+from nltk.corpus import stopwords
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    
+    y=[]
+    for i in text:
+        if i.isalnum:
+            y.append(i)
+    
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        y.append(ps.stem(i))
+            
+    return " ".join(y) 
